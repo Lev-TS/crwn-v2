4275e8507efa3c04a5773a88fe5d86fe813c7356
Lev-TS <57505924+Lev-TS@users.noreply.github.com>
2020-07-09 15:49:27 +0300

    adding custom sign in components

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import './App.css';
 // Router components
 import { Switch, Route } from 'react-router-dom'
 
-// Page component
+// Components persisting across all pages
+import Header from './components/header/header.component';
+
+// Page components
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 function App() {
 	return (
@@ -16,6 +19,7 @@ function App() {
 			<Switch>
 				<Route exact path='/' component={HomePage}/>
 				<Route path='/shop' component={ShopPage} />
+				<Route path='/signin' component={SignInAndSignUp} />
 			</Switch>
 		</div>
 	);
