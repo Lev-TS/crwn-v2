a85a0ecaa434d90c7e3f88f03cc31a244b9e02ac
Lev-TS <57505924+Lev-TS@users.noreply.github.com>
2020-08-05 15:03:35 +0300

    adding cart icon and cart dropdown component to the header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,8 +6,10 @@ import './header.styles.scss';
 
 import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
 	return (
 		<div className="header">
 			<Link className="logo-container" to="/">
@@ -29,7 +31,9 @@ const Header = ({ currentUser }) => {
 						SIGN IN
 					</Link>
 				)}
+				<CartIcon />
 			</div>
+			{hidden ? null : <CartDropdown />}
 		</div>
 	);
 };
@@ -40,8 +44,10 @@ const Header = ({ currentUser }) => {
 // in the Header component on line 10. The property name on the object
 // returned from the mapStateToProps function should always be the same
 // as the prop name we are destructuring in the component.
-const mapStateToProps = (state) => ({
-	currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+	// here we are destructuring nested properties
+	currentUser,
+	hidden,
 });
 
 // connect is the higher order function that tweaks the components we
---
+++ src/redux/cart/cart.actions.js
@@ -0,0 +1,8 @@
+import { CartActionTypes } from './cart.types';
+
+// it is not necessary to pass payload, our feature is
+// set up so that it will set the hidden property in
+// the reducer to opposite of whatever it's previous state was  
+export const toggleCartHidden = () => ({
+    type: CartActionTypes.TOGGLE_CART_HIDDEN
+})
