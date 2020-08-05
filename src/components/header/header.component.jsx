34ac716798b8e97d650141f554e07c2a7efb29be
Lev-TS <57505924+Lev-TS@users.noreply.github.com>
2020-08-05 19:52:30 +0300

    created cart-item component and connected cart dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,41 +2,40 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import './header.styles.scss';
-
 import { auth } from '../../firebase/firebase.utils';
-import { ReactComponent as Logo } from '../../assets/crown.svg';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser, hidden }) => {
-	return (
-		<div className="header">
-			<Link className="logo-container" to="/">
-				<Logo className="logo" />
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+import './header.styles.scss';
+
+const Header = ({ currentUser, hidden }) => (
+	<div className="header">
+		<Link className="logo-container" to="/">
+			<Logo className="logo" />
+		</Link>
+		<div className="options">
+			<Link className="option" to="/shop">
+				SHOP
 			</Link>
-			<div className="options">
-				<Link className="option" to="/shop">
-					SHOP
+			<Link className="option" to="/shop">
+				CONTACT
+			</Link>
+			{currentUser ? (
+				<div className="option" onClick={() => auth.signOut()}>
+					SIGN OUT
+				</div>
+			) : (
+				<Link className="option" to="/signin">
+					SIGN IN
 				</Link>
-				<Link className="option" to="/shop">
-					CONTACT
-				</Link>
-				{currentUser ? (
-					<div className="option" onClick={() => auth.signOut()}>
-						SIGN OUT
-					</div>
-				) : (
-					<Link className="option" to="/signin">
-						SIGN IN
-					</Link>
-				)}
-				<CartIcon />
-			</div>
-			{hidden ? null : <CartDropdown />}
+			)}
+			<CartIcon />
 		</div>
-	);
-};
+		{hidden ? null : <CartDropdown />}
+	</div>
+);
 
 // we are passing the rootReducer object (store) in this function,
 // and we are accessing user property on that store which was
@@ -45,7 +44,6 @@ const Header = ({ currentUser, hidden }) => {
 // returned from the mapStateToProps function should always be the same
 // as the prop name we are destructuring in the component.
 const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
-	// here we are destructuring nested properties
 	currentUser,
 	hidden,
 });
