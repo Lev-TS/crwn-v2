89c6f3faff935091d1e1a85cae9abd016f1ae248
Lev-TS <57505924+Lev-TS@users.noreply.github.com>
2020-08-17 23:13:45 +0300

    added addItem and removeItem functionality to the checkout-item component

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -5,9 +5,19 @@ import { CartActionTypes } from './cart.types';
 // the reducer to opposite of whatever it's previous state was  
 export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
-})
+});
 
 export const addItem = item => ({
     type: CartActionTypes.ADD_ITEM,
     payload: item
-})
+});
+
+export const removeItem = item => ({
+    type: CartActionTypes.REMOVE_ITEM,
+    payload: item
+});
+
+export const clearItemFromCart = item => ({
+    type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+    payload: item
+});
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -11,8 +11,26 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 		);
 	}
 
-    // this is when we attach quantity property to the cart item
+	// this is when we attach quantity property to the cart item
 	// object. quantity property gets attached the first time
 	// aroung since the above if block won't run when it's a new item
 	return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+	const existingCartItem = cartItems.find(
+		(cartItem) => cartItem.id === cartItemToRemove.id
+	);
+
+	if (existingCartItem.quantity === 1) {
+		return cartItems.filter(
+			(cartItem) => cartItem.id !== cartItemToRemove.id
+		);
+	}
+
+	return cartItems.map((cartItem) =>
+		cartItem.id === cartItemToRemove.id
+			? { ...cartItem, quantity: cartItem.quantity - 1 }
+			: cartItem
+	);
 };
