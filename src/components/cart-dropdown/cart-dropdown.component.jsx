03ddc95d6978964a45c5e8b4474bbc15f1a72462
Lev-TS <57505924+Lev-TS@users.noreply.github.com>
2020-08-17 15:41:42 +0300

    updating app with reselect, replaced cart selectors with memoized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -7,18 +7,18 @@ import CartItem from '../cart-item/cart-item.component';
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = ({ cartItems }) => (
-  <div className='cart-dropdown'>
-    <div className='cart-items'>
-      {cartItems.map(cartItem => (
-        <CartItem key={cartItem.id} item={cartItem} />
-      ))}
-    </div>
-    <CustomButton>GO TO CHECKOUT</CustomButton>
-  </div>
+	<div className="cart-dropdown">
+		<div className="cart-items">
+			{cartItems.map((cartItem) => (
+				<CartItem key={cartItem.id} item={cartItem} />
+			))}
+		</div>
+		<CustomButton>GO TO CHECKOUT</CustomButton>
+	</div>
 );
 
 const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems
+	cartItems,
 });
 
 export default connect(mapStateToProps)(CartDropdown);
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,26 @@
+import { createSelector } from 'reselect';
+
+// this input selector just gets the cart property
+// from the global state object
+const selectCart = (state) => state.cart;
+
+// this output selector takes two argument, the first
+// argument is an array of input selectors (there could
+// be more than one), the second argument is a function
+// that returns the value we won't from this selector.
+// since we used createSelector function. this is the
+// memoized output selector.
+export const selectCartItems = createSelector(
+	[selectCart],
+	(cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+	[selectCartItems],
+	(cartItems) =>
+		cartItems.reduce(
+			(accumulatedQuantity, cartItem) =>
+				accumulatedQuantity + cartItem.quantity,
+			0
+		)
+);
