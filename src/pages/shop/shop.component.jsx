1d85fd6f14d4a27bd1191fb24f6eca457b546e52
Lev-TS <57505924+Lev-TS@users.noreply.github.com>
2020-10-05 22:19:47 +0300

    adding shop and collection reducers, adding react persist functionality to persist cart items  between sessions

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import SHOP_DATA from './shop.data';
-import './shop.styles.scss';
+import "./shop.styles.scss";
 
-import CollectionPreview from '../../components/preview-collection/collection-preview.component.jsx';
+import CollectionOverview from '../../components/collections-overview/collections-overview.component'
 
-class ShopPage extends React.Component {
-	constructor(props) {
-		super(props);
+const ShopPage = ({ collections }) => (
+	<div className="shop-page">
+		<CollectionOverview/>
+	</div>
+);
 
-		this.state = {
-			collections: SHOP_DATA,
-		};
-	}
 
-	render() {
-		const { collections } = this.state;
-		return (
-			<div className="shop-page">
-				{collections.map(({id, ...otherCollectionProps}) => (
-					<CollectionPreview key={id} {...otherCollectionProps}/>
-				))}
-			</div>
-		);
-	}
-}
 
 export default ShopPage;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
+import logger from "redux-logger";
 
-import rootReducer from './root-reducer';
+import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
 // this is more scaleable verstion to create middlewares variable and
-// spread it out inside of applyMiddleware function, that's just in case we 
-// would need to add more middleware in the future. Otherwise, we could 
+// spread it out inside of applyMiddleware function, that's just in case we
+// would need to add more middleware in the future. Otherwise, we could
 // write it lie applyMiddleware(logger).
-const store = createStore(rootReducer, applyMiddleware(...middlewares))
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
