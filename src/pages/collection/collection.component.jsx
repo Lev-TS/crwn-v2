da20d8df02daeda92803e709243080045372b55a
Lev-TS <57505924+Lev-TS@users.noreply.github.com>
2020-10-11 12:12:00 +0300

    moved directory and shop data into its own reducer, creted respective selectors and updated corresponding components with new ux flow for directory/shop

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -8,16 +8,21 @@ import CollectionItem from "../../components/collection-item/collection-item.com
 import { selectCollection } from "../../redux/shop/shop.selectors";
 
 const CollectionPage = ({ collection }) => {
-	console.log(collection);
+	const { title, items } = collection;
 	return (
 		<div className="collection-page">
-			<h2>CATEGORY PAGE</h2>
+			<h2 className="title">{title}</h2>
+			<div className="items">
+				{items.map((item) => (
+					<CollectionItem key={item.id} item={item} />
+				))}
+			</div>
 		</div>
 	);
 };
 
 const mapStateToProps = (state, ownProps) => ({
-	collection: selectCollection(ownProps.match.params.collectionId)(state)
+	collection: selectCollection(ownProps.match.params.collectionId)(state),
 });
 
 export default connect(mapStateToProps)(CollectionPage);
